db94c421e0f701d7149290faf69781b9e4590667
Victor Assis <victor_assis@poli.ufrj.br>
2024-03-18 14:27:13 -0300

    feat: Project setup
    eslint + prettier + Tailwind config + Shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './global.css'
+
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <div>
+      <h1 className="bg-blue-500 underline">
+        App
+        <Button>Click me</Button>
+      </h1>
+    </div>
+  )
+}
